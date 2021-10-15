24600bf32704375fa400a677c986b74b3f895cc5
Evgenii Kosmogradskii-Ivanov <kosmogradsky@gmail.com>
2021-10-15 04:39:39 +0200

    start implementing session

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require("electron");
-const path = require('path');
+const path = require("path");
+const net = require("net");
+const rxjs = require("rxjs");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,8 +11,30 @@ function createWindow() {
 
   win.loadFile(path.resolve(__dirname, "index.html"));
   win.openDevTools();
+
+  return win;
+}
+
+function createSessionMapSubject(socket, win) {
+  const sessionMapSubject = new rxjs.BehaviorSubject(new Map());
+
+  // socket.on
+
+  sessionMapSubject.subscribe((sessionMap) => {
+    win.webContents.send(sessionMap);
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  const win = createWindow();
+
+  const server = net.createServer(function (socket) {
+    socket.write("Hello client");
+
+    socket.on("data", (data) => {
+      console.log(data);
+    });
+  });
+
+  server.listen(8080, "127.0.0.1");
 });
